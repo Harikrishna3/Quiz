3475fbfce4fe7e6f43b25ff6f96dd89dbc8e2d51
Harikrishna3 <harikrishnabbomen@gmail.com>
2023-04-19 18:31:44 +0530

    added next assignment button

--- index.js
+++ index.js
@@ -1453,17 +1453,6 @@ const quizData = [
       correctAnswer: "All of the above",
    },
    {
-      question:
-         "What is/are the major challenge(s) associated with the healthcare?",
-      answers: [
-         "Avoidable medical errors",
-         "Increasing age of the population",
-         "Costly hospitals and clinics",
-         "All of the above",
-      ],
-      correctAnswer: "All of the above",
-   },
-   {
       question: "What is/are the challenges associated with healthcare?",
       answers: [
          "Populations are aging all over the world",
@@ -1613,7 +1602,7 @@ const quizData = [
          "Diagnostic",
          "None of the above",
       ],
-      correctAnswer: "None of the above",
+      correctAnswer: "Predictive",
    },
    {
       question:
@@ -1844,15 +1833,37 @@ nextButton.addEventListener("click", () => {
 //    assg=14-n;
 //    currentQuestionIndex % 12==0 ? assg = 0 : assg++;
 //    currentQuestionIndex+=assg;
+//    console.log(currentQuestionIndex);
 //    var h2 = document.getElementById("assg");
 //    h2.innerHTML=`Assignment No ${q}`;
 //    if (currentQuestionIndex < quizData.length) {
-//       var h3 = document.getElementById("demo");
-//       h3.innerHTML = "";
+//       // var h3 = document.getElementById("demo");
+//       // h3.innerHTML = "";
 //       showQuestion(quizData[currentQuestionIndex]);
-//       submitButton.style.display = "inline-block";
-//       nextButton.style.display = "none";
+//       // submitButton.style.display = "inline-block";
+//       // nextButton.style.display = "none";
 //    } else {
 //       showResult();
 //    }
 // });
+
+function nextA(){
+   var assg=0,n=0,q=1;
+   q++;
+   n=currentQuestionIndex%15;
+   assg=14-n;
+   currentQuestionIndex % 12==0 ? assg = 0 : assg++;
+   currentQuestionIndex+=assg;
+   console.log(currentQuestionIndex);
+   var h2 = document.getElementById("assg");
+   h2.innerHTML=`Assignment No ${q}`;
+   if (currentQuestionIndex < quizData.length) {
+      // var h3 = document.getElementById("demo");
+      // h3.innerHTML = "";
+      showQuestion(quizData[currentQuestionIndex]);
+      // submitButton.style.display = "inline-block";
+      // nextButton.style.display = "none";
+   } else {
+      showResult();
+   }
+}
